d2a247831ec52a1f8518c284215f867d90ee2b39
Rajanikant Deshmukh <rajanikant.deshmukh@quipper.com>
2020-08-26 15:49:30 +0900

    Initial commit: Add boortstrap.sh

---
+++ .dotfiles/bootstrap.sh
@@ -0,0 +1,18 @@
+#! /bin/zsh
+# Requirements: git, GitHub SSH key Setup
+
+# Setup .dotfiles
+git clone --bare git@github.com:aruke/.dotfiles.git $HOME/.dotfiles
+function dotfiles {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .dotfiles-backup
+dotfiles checkout
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles.";
+  else
+    echo "Backing up pre-existing dotfiles.";
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
